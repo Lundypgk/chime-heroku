3956b2f2f582482cf480bb3e6e68fed37e5eaa90
shengliang <ahliang51@hotmail.com>
2017-02-07 15:23:32 +0000

    first commit

---
+++ server.js
@@ -0,0 +1,51 @@
+/**
+ * Module dependencies.
+ */
+
+var express = require('express'),
+  //routes = require('./routes'),
+  http = require('http'),
+  path = require('path'),
+  bodyParser = require('body-parser'),
+  favicon = require('serve-favicon'),
+  logger = require('morgan'),
+  methodOverride = require('method-override');
+
+var viewDirectory = path.join(__dirname, 'views');
+var app = express();
+
+app.set('port', process.env.PORT || 3000);
+app.set('views', __dirname + '/views');
+
+app.use(logger('dev'));
+app.use(bodyParser.urlencoded({
+  extended: false
+}));
+app.use(methodOverride('_method'));
+app.use(require('stylus').middleware(__dirname + '/views'));
+app.use(express.static(viewDirectory,{index:false,extensions:['html']}));
+
+
+if (app.get('env') == 'development') {
+  app.locals.pretty = true;
+}
+
+app.get('/', function(req, res) {
+ res.redirect('home.html');
+});
+
+app.get('/login', function(req, res) {
+  res.redirect('login.html')
+});
+
+
+//Catch all other routes
+app.get('/*', function(req, res) {
+ res.redirect('home.html');
+});
+
+
+
+http.createServer(app).listen(app.get('port'), function() {
+  console.log("Express server listening on port " + app.get('port'));
+});
